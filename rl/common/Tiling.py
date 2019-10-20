761edce8182f43241ffe33b42e610a79fadf6738
ali <akyegane@gmail.com>
2019-10-20 22:08:45 +0330

    add tiling

--- rl/common/Tiling.py
+++ rl/common/Tiling.py
@@ -1,6 +1,7 @@
 import numpy as np
 from matplotlib.lines import Line2D
 import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
 
 
 class Tiling:
@@ -10,32 +11,39 @@ class Tiling:
         self._number_of_tiling = number_of_tilling
         self._bin = bin
         self._offset_x, self._offset_y = offset
+        self._tiling = []
 
-    def create_tiling_grid(self, offset):
-        grid = [np.linspace(0, self._width, self._bin + 1)[1:] + self._offset_x + offset,
-                np.linspace(0, self._height, self._bin + 1)[1:] + self._offset_y + offset
-                ]
+    def create_tiling_grid(self, tiling_index):
+        grid_x = np.arange(0, self._width + self._bin + 1, self._bin) + self._offset_x + tiling_index
+        grid_y = np.arange(0, self._height + self._bin + 1, self._bin) + self._offset_y + tiling_index
+        grid = np.array([np.clip(grid_x, 0, self._width), np.clip(grid_y, 0, self._height)])
         return grid
 
     def create_tilings(self):
-        return [self.create_tiling_grid(offset=(index)) for index in range(self._number_of_tiling)]
+        self._tiling = [self.create_tiling_grid(tiling_index) for tiling_index in range(self._number_of_tiling)]
 
-    def visualize_tilings(self, tilings):
+    def tile_encode(self, x, y):
+        zero = np.zeros(self._width, self._height)
+        return [(int(np.digitize(x, grid[0])) - 1, int(np.digitize(y, grid[1])) - 1) for grid in self._tiling]
+
+    def visualize_tilings(self):
         """Plot each tiling as a grid."""
         prop_cycle = plt.rcParams['axes.prop_cycle']
         colors = prop_cycle.by_key()['color']
-        linestyles = ['-', '-', '-']
+        linestyles = ['-', '--', '-.', ':']
         legend_lines = []
 
         fig, ax = plt.subplots(figsize=(10, 10))
-        for i, grid in enumerate(tilings):
+        for i, grid in enumerate(self._tiling):
             for x in grid[0]:
                 l = ax.axvline(x=x, color=colors[i % len(colors)], linestyle=linestyles[i % len(linestyles)], label=i)
             for y in grid[1]:
-                l = ax.axhline(y=y, color=colors[i % len(colors)], linestyle=linestyles[i % len(linestyles)])
+                l = ax.axhline(y=y, color=colors[i % len(colors)], linestyle=linestyles[i % len(linestyles)],
+                               linewidth=1)
             legend_lines.append(l)
         ax.grid('off')
-        ax.legend(legend_lines, ["Tiling #{}".format(t) for t in range(len(legend_lines))], facecolor='white', framealpha=0.9)
+        ax.legend(legend_lines, ["Tiling #{}".format(t) for t in range(len(legend_lines))], facecolor='white',
+                  framealpha=0.9)
         ax.set_title("Tilings")
         plt.show()
-        return ax  # return Axis object to draw on later, if needed
+        return ax
--- test/tiling.py
+++ test/tiling.py
@@ -1,4 +1,6 @@
 from rl.common.Tiling import Tiling
 
-tiling = Tiling(12, 12, number_of_tilling=4, bin=4, offset=(0, -3))
-tiling.visualize_tilings(tiling.create_tilings())
+tiling = Tiling(10, 12, number_of_tilling=4, bin=4, offset=(-1, -1))
+tiling.create_tilings()
+tiling.visualize_tilings()
+print(tiling.tile_encode(5, 6))
