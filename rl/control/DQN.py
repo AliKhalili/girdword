f29ea473f2ca442c0b0bdcb508c64b71c73f65f8
ali <akyegane@gmail.com>
2019-11-09 16:01:27 +0330

    update DQN not complete version

--- rl/control/DQN.py
+++ rl/control/DQN.py
@@ -2,6 +2,7 @@ from collections import deque
 from keras.layers import Dense
 from keras.optimizers import Adam
 from keras.models import Sequential
+from keras.models import load_model
 from collections import namedtuple
 import random
 import numpy as np
@@ -27,32 +28,34 @@ class DQN(BaseControl):
         runs = {}
         for i in range(number_of_episode):
             state = self.env.reset()
-            action = policy.epsilon_greedy(self.exploration_rate, self.action_space, self.get_action_value(state))
             is_terminal = False
             while not is_terminal:
+                action = policy.epsilon_greedy(self.exploration_rate, self.action_space, self.get_action_value(state))
                 next_state, reward, is_terminal, info = self.env.step(CNSTNT.ACTIONS_VALUES[action])
-            self.add_transition(state, action, reward, next_state, is_terminal)
-            state = next_state
-            self.optimize_model()
+
+                self.add_transition(state, action, reward, next_state, is_terminal)
+                state = next_state
+                self.optimize_model()
+
             total_length, total_reward, _ = self.env.history()
             runs[i] = (total_length, total_reward)
         self.save_model()
         return runs
 
     def decode_state(self, state):
-        return self.env.get_position(state)
+        x, y = self.env.get_position(state)
+        return np.array([x, y]).reshape(1, 2)
 
     def get_action_value(self, state):
-        x, y = self.env.get_position(state)
-        return self._model.perdict([x, y])[0]
+        return self._model.predict(self.decode_state(state))[0]
 
     def add_transition(self, state, action, reward, next_state, done):
         self._memory.append(Transition(state, action, reward, next_state, done))
 
     def build_model(self):
         model = Sequential()
-        model.add(Dense(8, input_dim=2, activation='relu', kernel_initializer='he_uniform'))
-        model.add(Dense(8, activation='relu', kernel_initializer='he_uniform'))
+        model.add(Dense(24, input_dim=2, activation='relu', kernel_initializer='he_uniform'))
+        model.add(Dense(24, activation='relu', kernel_initializer='he_uniform'))
         model.add(Dense(self.action_space, activation='linear', kernel_initializer='he_uniform'))
         model.compile(loss='mse', optimizer=Adam(lr=self.learning_rate))
         return model
@@ -65,3 +68,25 @@ class DQN(BaseControl):
             target = transition.reward
             if not transition.done:
                 target = transition.reward + self.discount_factor * np.max(self.get_action_value(transition.next_state))
+            target_f = self._model.predict(self.decode_state(transition.state))
+            target_f[0][transition.action] = target
+            self._model.fit(self.decode_state(transition.state), target_f, epochs=1, verbose=0)
+
+    def save_model(self):
+        self._model.save(f'../result/{type(self).__name__}_Weights.h5')
+
+    def load_model(self):
+        self._model = load_model(f'../result/{type(self).__name__}_Weights.h5')
+
+    def evaluation(self, max_step=20):
+        self.load_model()
+        state = self.env.reset()
+        is_terminal = False
+        time = 0
+        while not is_terminal:
+            action = np.argmax(self._model.predict(self.decode_state(state))[0])
+            state, reward, is_terminal, info = self.env.step(CNSTNT.ACTIONS_VALUES[action])
+            time += 1
+            if time >= max_step:
+                break
+        self.env.save(name=type(self).__name__)
---
+++ test/dqn.py
@@ -0,0 +1,17 @@
+import os
+
+from rl.control.DQN import DQN
+from rl.control.QLearning import QLearning
+from rl.environment.mdp.MDPGrid import MDPGrid
+
+alpha = 0.1
+gamma = 1
+learning_rate =  0.001
+epsilon = 0.1
+number_of_episode = 10
+
+environment = MDPGrid(json_path=os.path.join(os.getcwd(), f'../env/env_10.json'))
+learner = DQN(environment, discount_factor=gamma, exploration_rate=epsilon, step_size=alpha,learning_rate=learning_rate)
+learner.run(number_of_episode)
+learner.evaluation()
+environment.save()
